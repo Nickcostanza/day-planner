68beb68e41d9e177fdacde155e87f902bdc99e38
Nick Costanza <nicholas.g.costanza@gmail.com>
2020-07-13 21:39:27 -0700

    added one time slot row for container.

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 //current date+time for header
 
-var current = moment().format('MMM Do YYYY');
+var current = moment().format('dddd, MMMM Do YYYY');
 $('#currentDay').text(current);
 
 console.log(current); 
+
+
+//add time slot to planner
+var hour = moment().format('h');
+console.log(hour)
+
+
+//create a function that changes colors of time slots as the day
+//goes on past/present/future
+
+
+//locally store the information put in to the time slots
+//refresh every morning
