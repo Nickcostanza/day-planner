a1a7c14a442de7a18e1d44a7eabbb79ced9ad25c
Nick Costanza <nicholas.g.costanza@gmail.com>
2020-07-14 13:35:33 -0700

    time slots added using moment.js

--- script.js
+++ script.js
@@ -6,13 +6,32 @@ $('#currentDay').text(current);
 console.log(current); 
 
 
-//add time slot to planner
-var hour = moment().format('h');
-console.log(hour)
+//add time slot to planner(SO MUCH EXTRA CODE..I KNOW Lol)
+var hourNine = moment().hour('9').format('h');
+var hourTen = moment().hour('10').format('h');
+var hourEleven = moment().hour('11').format('h');
+var hourTwelve = moment().hour('12').format('h');
+var hourOne = moment().hour('1').format('h');
+var hourTwo = moment().hour('2').format('h');
+var hourThree = moment().hour('3').format('h');
+var hourFour = moment().hour('4').format('h');
+var hourFive = moment().hour('5').format('h');
 
+var hours = [hourNine, hourTen, hourEleven, hourTwelve, hourOne, hourTwo, hourThree, hourFour, hourFive];
+
+$("#hourNine").text(hourNine + "AM");
+$("#hourTen").text(hourTen + "AM");
+$("#hourEleven").text(hourEleven + "AM");
+$("#hourTwelve").text(hourTwelve + "PM");
+$("#hourOne").text(hourOne + "PM");
+$("#hourTwo").text(hourTwo + "PM");
+$("#hourThree").text(hourThree + "PM");
+$("#hourFour").text(hourFour + "PM");
+$("#hourFive").text(hourFive + "PM");
 
 //create a function that changes colors of time slots as the day
 //goes on past/present/future
+
 
 
 //locally store the information put in to the time slots
